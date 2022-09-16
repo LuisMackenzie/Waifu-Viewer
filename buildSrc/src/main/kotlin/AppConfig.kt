8a9821d46d08b164fdf943c8dbee5b679aec1a37
LuisMackenzie <principemackenzie@gmail.com>
2022-09-16 18:49:20 +0200

    updated gradle dependency plugins

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,10 +2,10 @@
 object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 1
-    const val versionName = "0.1.1"
-    const val compileSdk = 32
+    const val versionName = "0.1.2"
+    const val compileSdk = 33
     const val minSdk = 23
-    const val targetSdk = 32
+    const val targetSdk = 33
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val testInstrumentationRunnerHilt = "com.mackenzie.waifuviewer.di.HiltTestRunner"
     // const val propertiesFileName = "secrets.properties"
