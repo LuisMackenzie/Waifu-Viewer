b55f34d183360e83d8c55df4efdc7c493482862b
LuisMackenzie <principemackenzie@gmail.com>
2022-09-16 17:29:37 +0200

    updated gradle dependency plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
 
 
     implementation ("androidx.core:core-ktx:1.8.0")
-    implementation ("androidx.appcompat:appcompat:1.5.0")
+    implementation ("androidx.appcompat:appcompat:1.5.1")
     implementation ("com.google.android.material:material:1.6.1")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
@@ -75,8 +75,8 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.5.2")
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
 
     // Corrutinas
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
