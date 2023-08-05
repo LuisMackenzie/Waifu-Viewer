2264d1f2261e1b6d10105fc1974236f5edbf3752
Mackenzie <principemackenzie@gmail.com>
2023-08-05 16:14:34 +0200

    refactoring a updating

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@
 object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 2
-    const val versionName = "0.5.2"
+    const val versionName = "0.5.3"
     const val compileSdk = 34
     const val minSdk = 23
     const val targetSdk = 34
