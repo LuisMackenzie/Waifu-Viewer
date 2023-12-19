33c6d7a60beb0734926c9da2b1436cb1572672de
Mackenzie <principemackenzie@gmail.com>
2023-12-19 19:09:56 +0100

    Fix deprecated getfromlocation

--- app/src/main/java/com/mackenzie/waifuviewer/data/PlayServicesLocationDataSource.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/data/PlayServicesLocationDataSource.kt
@@ -2,8 +2,11 @@ package com.mackenzie.waifuviewer.data
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.location.Address
 import android.location.Geocoder
 import android.location.Location
+import android.os.Build
+import android.util.Log
 import com.google.android.gms.location.LocationServices
 import com.mackenzie.waifuviewer.data.datasource.LocationDataSource
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -23,7 +26,29 @@ class PlayServicesLocationDataSource @Inject constructor(application: Applicatio
                 }
         }
 
+    @Suppress("DEPRECATION")
     private fun Location?.toRegion(): String? {
+        var newCC: String? = null
+        this?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                geocoder.getFromLocation(latitude, longitude, 1, object : Geocoder.GeocodeListener{
+                    override fun onGeocode(addresses: MutableList<Address>) {
+                        newCC = addresses.firstOrNull()?.countryCode
+                        Log.e("getFromLocation", "countryCode = $newCC")
+                    }
+                    override fun onError(errorMessage: String?) {
+                        super.onError(errorMessage)
+                        Log.e("getFromLocation", "Error= $errorMessage")
+                    }
+                })
+            } else {
+                newCC = geocoder.getFromLocation(latitude, longitude, 1)?.firstOrNull()?.countryCode
+            }
+        }
+        return newCC ?: "US"
+    }
+
+    private fun Location?.toRegion2(): String? {
         val addresses = this?.let {
             geocoder.getFromLocation(latitude, longitude, 1)
         }
