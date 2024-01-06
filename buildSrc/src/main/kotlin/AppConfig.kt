6e69f9df54c8e0801bc355640c08aadd12efc24e
Mackenzie <principemackenzie@gmail.com>
2024-01-06 16:29:30 +0100

    Update Dependencies

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@
 object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 2
-    const val versionName = "0.6.0"
+    const val versionName = "0.6.1"
     const val compileSdk = 34
     const val minSdk = 23
     const val targetSdk = 34
