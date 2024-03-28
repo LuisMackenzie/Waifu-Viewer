cfc5319f0d143212bafcee76cd018f490c206460
Mackenzie <principemackenzie@gmail.com>
2024-03-28 15:21:03 +0100

    Added function in Activity

--- app/src/main/java/com/mackenzie/waifuviewer/ui/NavHostActivity.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/NavHostActivity.kt
@@ -1,6 +1,7 @@
 package com.mackenzie.waifuviewer.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -14,6 +15,7 @@ class NavHostActivity : AppCompatActivity() {
     private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_nav_host)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         firebaseAnalytics = Firebase.analytics
