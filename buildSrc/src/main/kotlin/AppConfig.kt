052e23dbdf0fccfb68f1b860689480a77593a032
Mackenzie <principemackenzie@gmail.com>
2024-04-06 18:04:29 +0200

    updated versonName

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@
 object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 2
-    const val versionName = "0.7.0"
+    const val versionName = "0.7.1"
     const val compileSdk = 34
     const val minSdk = 26
     const val targetSdk = 34
