72b5ad6782431b9a86b2c39e354aafc957b91537
Mackenzie <principemackenzie@gmail.com>
2024-09-12 16:54:35 +0200

    Added compatibility with landscape completed

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@
 object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 1
-    const val versionName = "0.7.7"
+    const val versionName = "0.7.8"
     const val compileSdk = 34
     const val minSdk = 26
     const val targetSdk = 34
