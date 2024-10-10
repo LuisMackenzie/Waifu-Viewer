65e567e7c100988bfeb0ff00e3fdc5e6dcd0140d
Mackenzie <principemackenzie@gmail.com>
2024-10-10 18:21:09 +0200

    added launcher icon for flavors

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@
 object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 1
-    const val versionName = "0.7.12"
+    const val versionName = "0.7.13"
     const val compileSdk = 34
     const val minSdk = 26
     const val targetSdk = 34
