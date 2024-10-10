760b24c5ded9a53e53cf5fc7773e62e05ae14b88
Mackenzie <principemackenzie@gmail.com>
2024-10-10 19:01:41 +0200

    updated dependencies

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -3,9 +3,9 @@ object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 1
     const val versionName = "0.7.13"
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 26
-    const val targetSdk = 34
+    const val targetSdk = 35
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val testInstrumentationRunnerHilt = "com.mackenzie.waifuviewer.di.HiltTestRunner"
     // const val propertiesFileName = "secrets.properties"
