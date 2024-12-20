0de143c11cc5eb3c9fb8c0e5be13ab8a4ddb47c0
Mackenzie <principemackenzie@gmail.com>
2024-12-20 18:12:55 +0100

    fix behavior in tablets

--- app/src/main/java/com/mackenzie/waifuviewer/ui/common/ui/Utils.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/common/ui/Utils.kt
@@ -1,14 +1,23 @@
 package com.mackenzie.waifuviewer.ui.common.ui
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
+import com.mackenzie.waifuviewer.ui.common.isLandscape
 
 @Composable
 fun isNavigationBarVisible(): Boolean {
     val insets = LocalView.current.rootWindowInsets
     val navBarHeightPx = insets.stableInsetBottom
     val navBarHeightDp = with(LocalDensity.current) { navBarHeightPx.toDp() }
-    return navBarHeightDp > 20.dp
+    Log.e("isNavigationBarVisible", "navBarHeightDp: $navBarHeightDp")
+    // Para tabletas la barra de navegacion puede ser mayor
+    if (LocalContext.current.isLandscape()) {
+        return navBarHeightDp > 32.dp
+    } else {
+        return navBarHeightDp > 20.dp
+    }
 }
