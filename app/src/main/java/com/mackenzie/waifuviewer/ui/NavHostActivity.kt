5bb1105e97cd29f0c8d52843186df5831102c1f1
Mackenzie <principemackenzie@gmail.com>
2025-01-01 14:50:12 +0100

    delete last activity layout

--- app/src/main/java/com/mackenzie/waifuviewer/ui/NavHostActivity.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/NavHostActivity.kt
@@ -7,26 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
-import com.mackenzie.waifuviewer.R
-import com.mackenzie.waifuviewer.databinding.ActivityNavHostBinding
 import com.mackenzie.waifuviewer.ui.common.nav.Navigation
 import com.mackenzie.waifuviewer.ui.main.ui.MainTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NavHostActivity : AppCompatActivity(R.layout.activity_nav_host) {
+class NavHostActivity : AppCompatActivity() {
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
-    private lateinit var binding: ActivityNavHostBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNavHostBinding.inflate(layoutInflater)
         firebaseAnalytics = Firebase.analytics
         enableEdgeToEdge()
-
-        // Navigation Compose
-        // TODO doing
         setContent {
             MainTheme {
                 Navigation()
