8a7273ea13de6df60b5bf66fe9ce388110871719
mackenzie <principemackenzie@gmail.com>
2025-01-08 20:51:28 +0100

    trying to log events in firebase

--- app/src/main/java/com/mackenzie/waifuviewer/ui/NavHostActivity.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/NavHostActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.appcheck.ktx.appCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import com.mackenzie.waifuviewer.ui.common.getFirebaseInstance
 import com.mackenzie.waifuviewer.ui.common.nav.Navigation
 import com.mackenzie.waifuviewer.ui.main.ui.MainTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,11 +19,25 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NavHostActivity : AppCompatActivity() {
 
-    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    // private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    // open val firebaseAnalytics = getFirebaseInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        firebaseAnalytics = Firebase.analytics
+
+        // Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance(),
+        )
+
+        val crashlytics = Firebase.crashlytics
+        val analytics = Firebase.analytics
+        // analytics.setAnalyticsCollectionEnabled(true)
+        // crashlytics.setCrashlyticsCollectionEnabled(true)
+        // crashlytics.isCrashlyticsCollectionEnabled = true
+        // analytics.logEvent(FirebaseAnalytics.Event.PURCHASE, null)
+        // crashlytics.log("Activity created in develop Mode")
         enableEdgeToEdge()
         setContent {
             MainTheme {
