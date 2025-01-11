71802db128c92072347ab8f89c49a7bff5460605
Mackenzie <principemackenzie@gmail.com>
2025-01-11 15:46:13 +0100

    doing repo

--- data/src/main/java/com/mackenzie/waifuviewer/data/PushRepository.kt
+++ data/src/main/java/com/mackenzie/waifuviewer/data/PushRepository.kt
@@ -1,10 +1,15 @@
 package com.mackenzie.waifuviewer.data
 
+import com.mackenzie.waifuviewer.data.datasource.NotificationLocalDataSource
 import com.mackenzie.waifuviewer.data.datasource.TokenLocalDataSource
 import javax.inject.Inject
 
 class PushRepository@Inject constructor(
     private val tokenDataSource: TokenLocalDataSource,
-    // private val pushDataSource: PushLocalDataSource
+    private val pushDataSource: NotificationLocalDataSource
 ) {
+
+
+
+
 }
