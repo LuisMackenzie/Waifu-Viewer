378525ef321fcf0e52b614e50661fc9ecdc23509
mackenzie <principemackenzie@gmail.com>
2025-01-11 17:04:51 +0100

    fix DB objects

--- app/src/main/java/com/mackenzie/waifuviewer/data/db/dao/WaifuPushDao.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/data/db/dao/WaifuPushDao.kt
@@ -1,5 +1,6 @@
 package com.mackenzie.waifuviewer.data.db.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -7,6 +8,7 @@ import androidx.room.Query
 import com.mackenzie.waifuviewer.data.db.NotificationDb
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface WaifuPushDao {
 
     @Query("SELECT * FROM NotificationDb")
