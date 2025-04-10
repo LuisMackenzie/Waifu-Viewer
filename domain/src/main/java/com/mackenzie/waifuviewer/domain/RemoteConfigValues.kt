47d758fae11f19b841e2ebc09ff70345dd433132
Mackenzie <principemackenzie@gmail.com>
2025-04-10 19:36:50 +0200

    new value for app update checker

--- domain/src/main/java/com/mackenzie/waifuviewer/domain/RemoteConfigValues.kt
+++ domain/src/main/java/com/mackenzie/waifuviewer/domain/RemoteConfigValues.kt
@@ -5,6 +5,7 @@ data class RemoteConfigValues(
     val gptIsActive: Boolean = false,
     val geminiIsActive: Boolean = false,
     val AutoModeIsEnabled: Boolean = false,
+    var latestServerVersion: String? = null,
     var isFavorite: Boolean = false,
     var mode : Int = 0,
     var type: ServerType? = null
