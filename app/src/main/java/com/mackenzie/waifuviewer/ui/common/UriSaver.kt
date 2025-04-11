a631299ff0a20f54683ff6fcc2788a711ddbe4db
Mackenzie <principemackenzie@gmail.com>
2025-04-11 20:03:27 +0200

    new function for check version in server and take action.

--- app/src/main/java/com/mackenzie/waifuviewer/ui/common/UriSaver.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/common/UriSaver.kt
@@ -3,10 +3,11 @@ package com.mackenzie.waifuviewer.ui.common
 import android.net.Uri
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.SaverScope
+import androidx.core.net.toUri
 
 class UriSaver : Saver<MutableList<Uri>, List<String>> {
     override fun restore(value: List<String>): MutableList<Uri> = value.map {
-        Uri.parse(it)
+        it.toUri()
     }.toMutableList()
 
     override fun SaverScope.save(value: MutableList<Uri>): List<String> = value.map { it.toString() }
--- app/src/main/java/com/mackenzie/waifuviewer/ui/selector/ui/WaifuUpdateDialog.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/selector/ui/WaifuUpdateDialog.kt
@@ -1,16 +1,22 @@
 package com.mackenzie.waifuviewer.ui.selector.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.mackenzie.waifuviewer.BuildConfig
 import com.mackenzie.waifuviewer.R
 import com.mackenzie.waifuviewer.ui.common.Constants
+import com.mackenzie.waifuviewer.ui.common.removeVersionSuffix
 
 @Preview(showBackground = true)
 @Composable
@@ -22,12 +28,24 @@ fun WaifuUpdateDialog(
     AlertDialog(
         onDismissRequest = { onDismissRequest(false) },
         title = { Text(text = stringResource(id = R.string.dialog_update_title)) },
-        text = { Text(
-            text = stringResource(id = R.string.dialog_update_subtitle)
-                    + Constants.SPACE + BuildConfig.VERSION_NAME
-                    + stringResource(id = R.string.dialog_update_subtitle_hint)
-                    + Constants.SPACE + latestVersion
-        ) },
+        text = {
+            Column {
+                Text(text = stringResource(id = R.string.dialog_update_subtitle))
+                Row {
+                    Text(text = stringResource(id = R.string.dialog_update_local_version) )
+                    Text(
+                        text = Constants.SPACE + BuildConfig.VERSION_NAME.removeVersionSuffix(),
+                        color = Color.Black,
+                        // fontStyle = FontStyle.Italic,
+                        fontFamily = FontFamily.Monospace
+                    )
+                }
+                Row {
+                    Text(text = stringResource(id = R.string.dialog_update_latest_version) )
+                    Text(text = Constants.SPACE + latestVersion, color = Color.Red, fontStyle = FontStyle.Italic, fontFamily = FontFamily.Monospace)
+                }
+            }
+        },
         icon = { Image(painter = painterResource(id = R.drawable.ic_splash), contentDescription = "Update Icon") },
         confirmButton = {
             Button(onClick = {
