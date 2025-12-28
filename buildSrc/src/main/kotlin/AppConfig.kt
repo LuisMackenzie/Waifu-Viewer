cf19950802f15fcec96f558b1cc5162d6f25d51c
mackenzie <principemackenzie@gmail.com>
2025-12-28 12:57:07 +0100

    update Compile and Target

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -3,9 +3,9 @@ object AppConfig {
     const val applicationId = "com.mackenzie.waifuviewer"
     const val versionCode = 1
     const val versionName = "0.8.8.4"
-    const val compileSdk = 35
+    const val compileSdk = 36
     const val minSdk = 23
-    const val targetSdk = 35
+    const val targetSdk = 36
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val testInstrumentationRunnerHilt = "com.mackenzie.waifuviewer.di.HiltTestRunner"
     // const val propertiesFileName = "secrets.properties"
