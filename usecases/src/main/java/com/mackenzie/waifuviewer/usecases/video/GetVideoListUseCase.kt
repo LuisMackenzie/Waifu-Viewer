e537887ae3e44ec6d5a7bf7a812fe7b41d5dfd9d
Mackenzie <principemackenzie@gmail.com>
2026-01-11 16:42:30 +0100

    adited existing useCase

--- usecases/src/main/java/com/mackenzie/waifuviewer/usecases/video/GetVideoListUseCase.kt
+++ usecases/src/main/java/com/mackenzie/waifuviewer/usecases/video/GetVideoListUseCase.kt
@@ -8,6 +8,15 @@ import javax.inject.Inject
 
 class GetVideoListUseCase @Inject constructor(private val repo: VideoHubRepository) {
 
-    suspend operator fun invoke():  Either<Error, VideoListItem> = repo.requestDefaultVideoList()
+    suspend operator fun invoke(
+        page: Int?,
+        thumbsize: String?,
+        search: String?,
+        tags: List<String>?,
+        stars: List<String>?,
+        category: String?,
+        ordering: String?,
+        period: String?
+    ):  Either<Error, VideoListItem> = repo.requestVideoList(page, thumbsize, search, tags, stars, category, ordering, period)
 
 }
