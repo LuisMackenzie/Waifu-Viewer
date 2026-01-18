ca21b824580829f586fb89291d17def01a8ed4b2
mackenzie <principemackenzie@gmail.com>
2026-01-18 12:15:25 +0100

    bypass nav

--- app/src/main/java/com/mackenzie/waifuviewer/ui/common/nav/Navigation.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/common/nav/Navigation.kt
@@ -42,7 +42,13 @@ fun Navigation() {
     ) {
         composable(NavItem.SplashScreen) {
             SplashScreenRoute {
-                navController.navigate(route = NavItem.SelectorScreen.route) {
+                // Navegacion Principal desactivada temporalmente
+                /*navController.navigate(route = NavItem.SelectorScreen.route) {
+                    popUpTo(route = NavItem.SplashScreen.route) { inclusive = true }
+                }*/
+
+                // Navegacion directa a VideoHub para pruebas
+                navController.navigate(route = NavItem.VideoServersScreen.route) {
                     popUpTo(route = NavItem.SplashScreen.route) { inclusive = true }
                 }
             }
