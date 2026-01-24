32ab5d6415937177a184addf187d43a8b45c7b21
mackenzie <principemackenzie@gmail.com>
2026-01-24 21:31:01 +0100

    fixed background player

--- app/src/main/java/com/mackenzie/waifuviewer/ui/gpt/ui/VideoPlayerScreenContent.kt
+++ app/src/main/java/com/mackenzie/waifuviewer/ui/gpt/ui/VideoPlayerScreenContent.kt
@@ -1,6 +1,7 @@
 package com.mackenzie.waifuviewer.ui.gpt.ui
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
@@ -12,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
@@ -86,7 +88,9 @@ fun VideoPlayerScreenContent(
             Log.e("VideoHubScreenContent", "embeddedVideoFile=${state.embeddedVideoFile}")
             // 5. Integrar el PlayerView de ExoPlayer usando AndroidView
             AndroidView(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .background(Color.Black)
+                    .fillMaxSize(),
                 factory = {
                     PlayerView(it).apply {
                         player = exoPlayer
